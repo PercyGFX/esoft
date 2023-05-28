1c7fc59effc44f55181ecb500dd6cb63e004cd4c
cflkdeveloper <cflkdeveloper@gmail.com>
2023-05-28 11:12:14 +0530

    Update about.php and index

---
+++ about.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+
+</head>
+
+<body class="container">
+
+    <!-- Top navigation-->
+
+    <div class="row">
+
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="index.html">EMC Phone</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">About Us</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+
+    </div>
+    <div class="container d-flex justify-content-center">
+        <span class="m-5 w-50">
+
+            <div class="border row p-5">
+                <h1 class="display-1 text-center">About Us</h1>
+                <div class="mb-3 row justify-content-center mt-5">
+                    <img src="./images/LOGO.png" class="justify-content-center col-2 w-50 m-3" alt="about">
+                </div>
+                <p class="text-justify mb-5">
+                    Established in 2020, EMC Mobile Store has strived to be one of the leading retailers for branded & customizable computers and related products in today’s market. Our many years of experience has provided us with the expertise to cater you; our valued customers with the latest technology, while providing an excellent service that would culminate in providing you the best available products. EMC Mobile Store has always been the stable backdrop for many PC enthusiasts in the face of rising enthusiasm for high-end computer gaming and custom-built PCs. Thus, enabling the dreams of making one's own computer that would fit all of one's needs come true.
+                </p>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4729.958079371943!2d80.54325556253863!3d5.948672087999741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2slk!4v1685251651468!5m2!1sen!2slk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+            </div>
+        </span>
+    </div>
+
+</body>
+
+</html>
