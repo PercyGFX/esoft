09836bcc7a189c166cedd5e4ceff846b1210b0c3
cflkdeveloper <cflkdeveloper@gmail.com>
2023-05-28 10:23:50 +0530

    Update dashboard.php

---
+++ admin/dashboard.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> EMC Mobile Store</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+
+</head>
+
+<body class="">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand">Store</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">ADD Product <span class="sr-only"></span></a>
+                    </li>
+                    <li class=" my-2 my-lg-0">
+                        <a class="nav-link" href="#">Log out</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="card m-5" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="..." id="image" name="image">
+                <div class="card-body">
+                    <h5 class="card-title" id="title" name="title">title</h5>
+                    <p class="card-text" id="desctption" name="desctption">description</p>
+                    <a href="#" class="btn btn-primary">view //navigate product page</a>
+                </div>
+            </div>
+            <div class="card m-5" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="..." id="image" name="image">
+                <div class="card-body">
+                    <h5 class="card-title" id="title" name="title">title</h5>
+                    <p class="card-text" id="desctption" name="desctption">description</p>
+                    <a href="#" class="btn btn-primary">view //navigate product page</a>
+                </div>
+            </div>
+            <div class="card m-5" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="..." id="image" name="image">
+                <div class="card-body">
+                    <h5 class="card-title" id="title" name="title">title</h5>
+                    <p class="card-text" id="desctption" name="desctption">description</p>
+                    <a href="#" class="btn btn-primary">view //navigate product page</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
